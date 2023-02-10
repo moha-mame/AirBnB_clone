5e963cedab56a2b76635953a4f02edb94069b1c0
moha-mame <>
2023-02-10 17:14:31 +0000

    AirBnB_clone-docm

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+"""Amenity Module"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-  
+    """
+    The public class attribute should return an empty string
+    """
     name = ""
--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-
+"""
+BaseModel - Module
+Parent class to take care of the initialization,
+serialization and deserialization of instances
+ """
 import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel:
-
+    """
+    BaseModel class Parent class to take care of the initialization,
+    serialization and deserialization of instances
+    """
     def __init__(self, *args, **kwargs):
-
+        """Initialization of a BaseModel instance"""
         if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -24,18 +31,18 @@ class BaseModel:
                     setattr(self, key, val)
 
     def __str__(self):
-
+        """String representation of a BaseModel instance"""
         return ("[{}] ({}) {}".format(self.__class__.__name__,
-          
+
                                       self.id, self.__dict__))
 
     def save(self):
-
+        """updates 'updated_at' instance with current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-
+        """Return dictionary representation of BaseModel class."""
         new_dict = dict(self.__dict__)
         new_dict['created_at'] = self.__dict__['created_at'].isoformat()
         new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
