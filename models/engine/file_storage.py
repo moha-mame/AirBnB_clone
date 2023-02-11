136fece81df01516eb4903c2e9f02269173ea9e1
moha-mame <>
2023-02-11 14:24:07 +0000

    AirBnB_clone_ update-file-sto

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-
+"""
+    Define class FileStorage
+"""
 import json
-import models
 
 
 class FileStorage:
-
+    """
+        Serializes instances to JSON file and deserializes to JSON file.
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-
+        """
+            Return the dictionary
+        """
         return self.__objects
 
     def new(self, obj):
-
+        """
+        Set new obj into __objects
+        """
         key = str(obj.__class__.__name__) + "." + str(obj.id)
         value_dict = obj
         FileStorage.__objects[key] = value_dict
 
     def save(self):
-
+        """
+        Serializes the objects into JSON file
+        """
         objects_dict = {}
         for key, val in FileStorage.__objects.items():
             objects_dict[key] = val.to_dict()
@@ -29,9 +38,12 @@ class FileStorage:
             json.dump(objects_dict, fd)
 
     def reload(self):
+        """
+        Reload the file and deserializes JSON into __objects
+        """
 
         try:
-            with open(FileStorage.__file_path, encoding="UTF8") as fd: #the file descriptor for which a file object is to be returned
+            with open(FileStorage.__file_path, encoding="UTF8") as fd:
                 FileStorage.__objects = json.load(fd)
             for key, val in FileStorage.__objects.items():
                 class_name = val["__class__"]
