171a3501ec1a3a93cdc1ebfaebda3007a762cce6
moha-mame <>
2023-02-13 08:50:24 +0000

    AirBnB_clone

--- console.py
+++ console.py
@@ -24,13 +24,13 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """Quit command to exit the program.
+        """ Quit command to exit the program.
         """
         return True
 
     def do_EOF(self, line):
         """ Exit the program."""
-        print()
+        print("")
         return True
 
     def emptyline(self):
